0b64a374e00a443db1dada5f089baba2be40f1fd
Matt Keeley <mrkeeley@asu.edu>
2021-01-21 18:22:21 -0700

    Create server.py

---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+
+
+def index():
+    return f'''
+    <h1><center>Welcome to the training grounds.</center></h1>
+    <h3><center>The source code for the target machine can be found <a href="/code">here</a></center></h3>
+    <h3><center>The attack page containing the source code can be found <a href="/attack">here</a></center></h3>
+    '''
+
+f = open("code.txt","r")
+source_code = f.read()
+
+@app.route('/code')
+def code():
+    return source_code.replace('<?php','')
+
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=80)
